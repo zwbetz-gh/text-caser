00c164e70e9464c628ca67bc2086c13d3ade3825
zwbetz-gh <zwbetz@gmail.com>
2020-11-11 11:07:26 -0600

    init bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Container, Row, Col, Form} from 'react-bootstrap';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col><h1>Text Utils</h1></Col>
+      </Row>
+      <Form>
+        <Row>
+          <Col>
+            <Form.Group controlId="formAction">
+              <Form.Label>Action</Form.Label>
+              <Form.Control as="select">
+              <option>Uppercase</option>
+              <option>Lowercase</option>
+              <option>Title case</option>
+              {/* More */}
+              </Form.Control>
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form.Text as="textarea" style={{
+              width: '100%'
+            }} />
+          </Col>
+        </Row>
+      </Form>
+    </Container>
   );
 }
 
